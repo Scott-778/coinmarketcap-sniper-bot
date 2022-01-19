6837606b8716ed572df9f387838eed40fd2feac0
Scott <91510798+Scott-778@users.noreply.github.com>
2022-01-19 12:32:00 -0700

    Auto sell feature with take profit and stop loss

--- cmcBotBUSD.js
+++ cmcBotBUSD.js
@@ -6,8 +6,6 @@ https://t.me/joinchat/b17jE6EbQX5kNWY8 use this link and subscribe.
 Turn on two step verification in telegram.
 Go to my.telegram.org and create App to get api_id and api_hash.
 If this helped you buy me a cup of coffee 0x17CCCc30297bCC1287943ea1bb549fF843878669
-
-This version buys both Coinmarketcap and Coingecko tokens with BUSD, you will still need some BNB for gas fees.
 */
 
 const {TelegramClient } = require("telegram"); //npm install telegram
@@ -15,10 +13,13 @@ const { StringSession } = require("telegram/sessions");
 const input = require("input"); // npm install input
 const { NewMessage } = require('telegram/events')
 const ethers = require('ethers'); // npm install ethers
+
 /* ----------------------------------- */
-const apiId = ;   
-const apiHash = ''; 
+
+const apiId = ; 
+const apiHash = '';
 const stringSession = new StringSession(""); // fill this later with the value from session.save()
+
 const addresses = {
 WBNB: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
 BUSD: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
@@ -26,11 +27,17 @@ pancakeRouter: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
 recipient: '' // Your wallet address
 }
 const mnemonic = ''; // Your wallet seed phrase
-const myGasPrice = ethers.utils.parseUnits('6', 'gwei'); // Adjust your gas here, the higher the better
+const myGasPrice = ethers.utils.parseUnits('5', 'gwei'); // Adjust your gas here, the higher the better
+const myGasPriceForApproval = ethers.utils.parseUnits('5', 'gwei');
 const myGasLimit = 1000000; // Gas limit 
 
-const maxTax = 10;
-const investmentAmount = '1.2'; // The amount you want to buy in BUSD
+const investmentAmount = '1.5'; // The amount you want to buy in BUSD
+const maxTax = 10; // The maximum tax for token you want to buy 
+
+const profitXAmount = 1.50; // take 50% profit with max tax accounted for.
+const stopLossXAmount = 0.90; // 10% loss with max tax accounted for. 
+const autoSell = true;  // false to turn off auto sell
+
 /* ----------------------------------- */
 
 const channelId = 1517585345;
@@ -48,29 +55,121 @@ pancakeAbi,
 account
 );
 
+let tokenAbi = [
+'function approve(address spender, uint amount) public returns(bool)',
+'function balanceOf(address account) external view returns (uint256)',
+'event Transfer(address indexed from, address indexed to, uint amount)'
+];
+var sellCount = 0;
+var buyCount = 0;
+
 const buy = async (path) =>{
-	console.log('Buying Token Now');
-	const amounts = await pancakeRouter.getAmountsOut(amountIn, path);
-	const amountOutMin = amounts[1].sub(amounts[1].div(2)); // 50% Slippage should be high enough so the transaction will not fail. 
-	//To change slippage tolerance change the last number on the line above. For example, 5 would be 20% slippage, 10 would be 10%, 2 would be 50%.  
+	if(buyCount == 0){
+		buyCount++;
+		console.log('Buying Token Now');
+		const amounts = await pancakeRouter.getAmountsOut(amountIn, path);
+		const amountOutMin = amounts[1].sub(amounts[1].div(2)); // 50% Slippage should be high enough so the transaction will not fail. 
+		//To change slippage tolerance change the last number on the line above. For example, 5 would be 20% slippage, 10 would be 10%, 2 would be 50%.  
 		
-	const tx = await pancakeRouter.swapExactTokensForTokens(
-		amountIn,
-		amountOutMin,
-		path,
-		addresses.recipient,
-		Math.floor(Date.now() / 1000) + 60 * 4, 
-		{
-		gasPrice: myGasPrice,
-		gasLimit: myGasLimit
-		}
+		const tx = await pancakeRouter.swapExactTokensForTokens(
+			amountIn,
+			amountOutMin,
+			path,
+			addresses.recipient,
+			Math.floor(Date.now() / 1000) + 60 * 4, 
+			{
+			gasPrice: myGasPrice,
+			gasLimit: myGasLimit
+			}
 		);
 		const receipt = await tx.wait();
 		console.log('Transaction receipt');
 		console.log(receipt);
-		process.exit(); 
-	
+		approve(path); 
+	}
 } 
+
+const approve = async (path) =>{
+	let contract = new ethers.Contract(tokenOut, tokenAbi, account);
+	const valueToApprove = ethers.constants.MaxUint256;
+	const tx = await contract.approve(
+		pancakeRouter.address, 
+		valueToApprove,
+		{
+         	 gasPrice: myGasPriceForApproval,
+         	 gasLimit: 210000,
+		}
+   	 );
+	const receipt = await tx.wait(); 
+	console.log(receipt);
+	if (autoSell){
+		checkForProfit(path);
+	}else{
+		process.exit();
+	}	
+}
+
+const checkForProfit = async(path) =>{
+	let tokenContract = new ethers.Contract(tokenOut, tokenAbi, account);
+	const takeProfit = (profitXAmount + maxTax / 100) * 100;
+	const takeLoss = (stopLossXAmount - maxTax / 100) * 100;
+	tokenContract.on("Transfer", async(from, to, value, event) => {
+		let bal = await tokenContract.balanceOf(addresses.recipient);
+		const amount = await pancakeRouter.getAmountsOut(bal,path.reverse());
+		const profitDesired = amountIn.mul(takeProfit).div(100);
+		const stopLoss = amountIn.mul(takeLoss).div(100);
+		let currentValue;
+		if(path.length == 3){
+			currentValue = amount[2];
+		}else{
+			currentValue = amount[1];
+		}
+		console.log('--- Current Value in BUSD:', ethers.utils.formatUnits(currentValue),'--- Profit At:', ethers.utils.formatUnits(profitDesired), '--- Stop Loss At:', ethers.utils.formatUnits(stopLoss), '\n');
+		if (currentValue.gte(profitDesired)){
+			if(sellCount == 0){
+				sellCount++;
+				console.log("Selling token profit target reached");
+				sell(path);
+			}
+		}
+		if (currentValue.lte(stopLoss)){
+			if(sellCount == 0){
+				sellCount++;
+				console.log("Selling token now stop loss reached");
+				sell(path);
+			}
+		}
+	}); 
+}
+
+
+
+const sell = async (path) =>{
+	const pathReversed = path.reverse();
+	try{
+		console.log('selling tokens');
+		let contract = new ethers.Contract(tokenOut, tokenAbi, account);
+		let bal = await contract.balanceOf(addresses.recipient);
+		const sellAmount = await pancakeRouter.getAmountsOut(bal, pathReversed);
+		const sellAmountsOutMin = sellAmount[1].sub(sellAmount[1].div(2));
+		const tx = await pancakeRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+			sellAmount[0].toString(),
+			sellAmountsOutMin,
+			pathReversed,
+			addresses.recipient,
+			Math.floor(Date.now() / 1000) + 60 * 3, 
+			{
+			gasPrice: myGasPriceForApproval,
+			gasLimit: myGasLimit,
+			}
+		);
+		const receipt = await tx.wait(); 
+		console.log(receipt);
+		process.exit();
+	}catch(e){
+		console.log(e)
+	}		
+}
 
 (async () => {
 	const client = new TelegramClient(stringSession, apiId, apiHash, {
@@ -84,6 +183,7 @@ const buy = async (path) =>{
   });
 	console.log("You should now be connected.");
         console.log(client.session.save()); // Save this string to avoid logging in again
+	console.log("Waiting for telegram notification to buy tokens...");
         client.addEventHandler(onNewMessage, new NewMessage({}));
 })();
 
@@ -110,7 +210,6 @@ async function onNewMessage(event) {
 				}
 			}
 		}
-		
 		if(shouldBuy && msg.includes('BNB')){  
 			console.log(address);
 			tokenOut = address;
@@ -124,3 +223,4 @@ async function onNewMessage(event) {
 		}
 	}
 }
+
